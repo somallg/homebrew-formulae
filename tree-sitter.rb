4d97f3664c0708db3198e8c5ec63dd9193162633
somallg <tq.duong@icloud.com>
2022-01-23 16:49:20 +0700

    update to match upstrea

--- tree-sitter.rb
+++ tree-sitter.rb
@@ -1,45 +1,44 @@
 # frozen_string_literal: true
 
-require 'language/node'
+require "language/node"
 
-# TreeSitter v0.19.5
 class TreeSitter < Formula
-  desc 'Parser generator tool and incremental parsing library'
-  homepage 'https://tree-sitter.github.io/'
-  url 'https://github.com/tree-sitter/tree-sitter/archive/v0.19.5.tar.gz'
-  sha256 '953edda5a21423cbfd916dbb1b1552a12f7ab649728846b5a1fb49483672e82b'
-  license 'MIT'
-  head 'https://github.com/tree-sitter/tree-sitter.git'
+  desc "Parser generator tool and incremental parsing library"
+  homepage "https://tree-sitter.github.io/"
+  url "https://github.com/tree-sitter/tree-sitter/archive/v0.19.5.tar.gz"
+  sha256 "953edda5a21423cbfd916dbb1b1552a12f7ab649728846b5a1fb49483672e82b"
+  license "MIT"
+  head "https://github.com/tree-sitter/tree-sitter.git"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: '0603c56e132ccd652a5eb07ebf951315590aa970e4d8a8571f884ac02ddf614f'
-    sha256 cellar: :any, big_sur:       '613460d86e0ed0472d024a0f9cf2fabb7da11f2487e96f00d1a9030f1bb29a55'
-    sha256 cellar: :any, catalina:      '5fe4891129581e6e910a6a4a6e9d57a163a0a06a25db66e84db65a367144a6e9'
-    sha256 cellar: :any, mojave:        'd76b4f8b94b91201d2e95de5254964ea4ca66421f3e801947a5bff31ddd6baf8'
+    sha256 cellar: :any, arm64_big_sur: "0603c56e132ccd652a5eb07ebf951315590aa970e4d8a8571f884ac02ddf614f"
+    sha256 cellar: :any, big_sur:       "613460d86e0ed0472d024a0f9cf2fabb7da11f2487e96f00d1a9030f1bb29a55"
+    sha256 cellar: :any, catalina:      "5fe4891129581e6e910a6a4a6e9d57a163a0a06a25db66e84db65a367144a6e9"
+    sha256 cellar: :any, mojave:        "d76b4f8b94b91201d2e95de5254964ea4ca66421f3e801947a5bff31ddd6baf8"
   end
 
-  depends_on 'emscripten' => %i[build test]
-  depends_on 'node' => %i[build test]
-  depends_on 'rust' => :build
+  depends_on "emscripten" => [:build, :test]
+  depends_on "node" => [:build, :test]
+  depends_on "rust" => :build
 
   def install
-    system 'make'
-    system 'make', 'install', "PREFIX=#{prefix}"
+    system "make"
+    system "make", "install", "PREFIX=#{prefix}"
 
     # NOTE: This step needs to be done *before* `cargo install`
-    cd 'lib/binding_web' do
-      system 'npm', 'install', *Language::Node.local_npm_install_args
+    cd "lib/binding_web" do
+      system "npm", "install", *Language::Node.local_npm_install_args
     end
-    system 'script/build-wasm'
+    system "script/build-wasm"
 
-    cd 'cli' do
-      system 'cargo', 'install', *std_cargo_args
+    cd "cli" do
+      system "cargo", "install", *std_cargo_args
     end
 
     # Install the wasm module into the prefix.
     # NOTE: This step needs to be done *after* `cargo install`.
     %w[tree-sitter.js tree-sitter-web.d.ts tree-sitter.wasm package.json].each do |file|
-      (lib / 'binding_web').install "lib/binding_web/#{file}"
+      (lib/"binding_web").install "lib/binding_web/#{file}"
     end
   end
 
@@ -48,7 +47,7 @@ class TreeSitter < Formula
     assert_equal "tree-sitter #{version}", shell_output("#{bin}/tree-sitter --version").strip
 
     # test `tree-sitter generate`
-    (testpath / 'grammar.js').write <<~EOS
+    (testpath/"grammar.js").write <<~EOS
       module.exports = grammar({
         name: 'YOUR_LANGUAGE_NAME',
         rules: {
@@ -56,17 +55,17 @@ class TreeSitter < Formula
         }
       });
     EOS
-    system bin / 'tree-sitter', 'generate'
+    system bin/"tree-sitter", "generate"
 
     # test `tree-sitter parse`
-    (testpath / 'test/corpus/hello.txt').write <<~EOS
+    (testpath/"test/corpus/hello.txt").write <<~EOS
       hello
     EOS
     parse_result = shell_output("#{bin}/tree-sitter parse #{testpath}/test/corpus/hello.txt").strip
-    assert_equal('(source_file [0, 0] - [1, 0])', parse_result)
+    assert_equal("(source_file [0, 0] - [1, 0])", parse_result)
 
     # test `tree-sitter test`
-    (testpath / 'test' / 'corpus' / 'test_case.txt').write <<~EOS
+    (testpath/"test"/"corpus"/"test_case.txt").write <<~EOS
       =========
         hello
       =========
@@ -74,9 +73,9 @@ class TreeSitter < Formula
       ---
       (source_file)
     EOS
-    system "#{bin}/tree-sitter", 'test'
+    system "#{bin}/tree-sitter", "test"
 
-    (testpath / 'test_program.c').write <<~EOS
+    (testpath/"test_program.c").write <<~EOS
       #include <string.h>
       #include <tree_sitter/api.h>
       int main(int argc, char* argv[]) {
@@ -102,11 +101,11 @@ class TreeSitter < Formula
         return 0;
       }
     EOS
-    system ENV.cc, 'test_program.c', "-L#{lib}", '-ltree-sitter', '-o', 'test_program'
-    assert_equal 'tree creation failed', shell_output('./test_program')
+    system ENV.cc, "test_program.c", "-L#{lib}", "-ltree-sitter", "-o", "test_program"
+    assert_equal "tree creation failed", shell_output("./test_program")
 
     # test `tree-sitter build-wasm`
-    ENV.delete 'CPATH'
-    system bin / 'tree-sitter', 'build-wasm'
+    ENV.delete "CPATH"
+    system bin/"tree-sitter", "build-wasm"
   end
 end
